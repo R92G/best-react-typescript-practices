695b1f48030c07fc3224ffdb569ec1560147ee0e
Robin <robinlaurentius@gmail.com>
2024-08-18 18:44:18 +0200

    feat: create reusable styled Container component for responsiveness

---
+++ src/components/Container.tsx
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+
+interface ContainerProps {
+  paddingTop?: string;
+}
+
+export const Container = styled.div<ContainerProps>`
+  width: 100%;
+  margin-left: auto;
+  margin-right: auto;
+  padding-left: 1rem;
+  padding-right: 1rem;
+
+  /* Dynamisch padding-top op basis van prop */
+  padding-top: ${(props) => props.paddingTop || "4rem"};
+
+  padding-bottom: 1rem;
+
+  @media (min-width: 768px) {
+    flex-direction: row; /* Switch to row on tablet and larger screens */
+    justify-content: space-between;
+  }
+
+  /* Standaard responsive breedtes voor verschillende schermgroottes */
+  max-width: 100%;
+
+  @media (min-width: 640px) {
+    max-width: 640px;
+  }
+
+  @media (min-width: 768px) {
+    max-width: 768px;
+  }
+
+  @media (min-width: 1024px) {
+    max-width: 1024px;
+  }
+
+  @media (min-width: 1280px) {
+    max-width: 1280px;
+  }
+
+  @media (min-width: 1536px) {
+    max-width: 1536px;
+  }
+`;
