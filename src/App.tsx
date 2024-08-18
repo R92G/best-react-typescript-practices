4ca7e72e2d9db3f1812f5a3cb2f1c979cf517a71
Robin <robinlaurentius@gmail.com>
2024-08-18 17:27:12 +0200

    feat: set up routing in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import CategoryPage from "./pages/CategoryPage";
+import ProductDetailPage from "./pages/ProductDetailPage";
+import ProductsPage from "./pages/ProductsPage";
 
 const App: React.FC = () => {
-  return <div>Initial Commit</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<CategoryPage />} />
+        <Route path="/categories" element={<CategoryPage />} />
+        <Route path="/categories/:categorySlug" element={<ProductsPage />} />
+        <Route path="/products/:productSlug" element={<ProductDetailPage />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
