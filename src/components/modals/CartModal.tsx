d2a2d4040e60ab74f62b77b0ccfa565b4170fb9d
Robin <robinlaurentius@gmail.com>
2024-08-19 12:19:54 +0200

    refactor: Update CartModal to display cart items with ProductListItem

--- src/components/modals/CartModal.tsx
+++ src/components/modals/CartModal.tsx
@@ -1,14 +1,29 @@
 import useCartStore from "../../stores/useCartStore";
 import { Modal } from "./Modal";
+import { ProductListItem } from "../features/ProductListItem";
 import useCartModal from "../../stores/useCartModal";
 
 export const CartModal = () => {
   const cartModal = useCartModal();
-  const { cartItems } = useCartStore();
+  const cartItems = useCartStore((state) => state.cartItems);
 
   return (
     <Modal title="Cart" isOpen={cartModal.isOpen} onClose={cartModal.closeCart}>
-      {cartItems.length > 0 ? <p>Cart has items</p> : <p>Your cart is empty</p>}
+      {cartItems.length > 0 ? (
+        <ul>
+          {cartItems.map((item) => (
+            <ProductListItem
+              key={item.modelCode}
+              thumbUrl={item.thumbUrl}
+              name={item.displayName}
+              price={(Number(item.price) * item.quantity).toFixed(2)}
+              quantity={item.quantity}
+            />
+          ))}
+        </ul>
+      ) : (
+        <p>Your cart is empty</p>
+      )}
     </Modal>
   );
 };
