451a330fe560b75f6955f8e6c0ce2ac1ca525d9e
Robin <robinlaurentius@gmail.com>
2024-08-19 10:04:42 +0200

    docs(hooks): add comments to useProductsByCategory hook

--- src/hooks/useProductsByCategory.tsx
+++ src/hooks/useProductsByCategory.tsx
@@ -3,11 +3,18 @@ import { useFetchProducts } from "../api/useFetchProducts";
 import { useParams } from "react-router-dom";
 import slugify from "slugify";
 
+/**
+ * Custom hook to fetch products by category.
+ *
+ * @returns {Object} contains products, isLoading and error
+ */
 export const useProductsByCategory = () => {
+  // Get the categorySlug from the URL
   const { categorySlug } = useParams<{ categorySlug: string }>();
 
   const { data, isLoading, error } = useFetchProducts();
 
+  // Filter products based on category
   const products = useMemo(() => {
     if (isLoading || !data || !categorySlug) {
       return [];
@@ -18,6 +25,7 @@ export const useProductsByCategory = () => {
         slugify(category.categorySubTypeName, { lower: true }) === categorySlug
     );
 
+    // Combine all products from all categories
     const allProducts = filteredData.flatMap((category) => category.modelList);
 
     return allProducts;
