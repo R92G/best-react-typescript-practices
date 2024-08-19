86ccb1900978308f3e8d489f6a8d1660f54a55be
Robin <robinlaurentius@gmail.com>
2024-08-19 10:03:15 +0200

    feat: add filters and useMemo to optimize useProductsByCategory hook for better performance

--- src/hooks/useProductsByCategory.tsx
+++ src/hooks/useProductsByCategory.tsx
@@ -1,10 +1,27 @@
+import { useMemo } from "react";
 import { useFetchProducts } from "../api/useFetchProducts";
 import { useParams } from "react-router-dom";
+import slugify from "slugify";
 
 export const useProductsByCategory = () => {
   const { categorySlug } = useParams<{ categorySlug: string }>();
 
   const { data, isLoading, error } = useFetchProducts();
 
-  return { data, isLoading, error, categorySlug };
+  const products = useMemo(() => {
+    if (isLoading || !data || !categorySlug) {
+      return [];
+    }
+
+    const filteredData = data.filter(
+      (category) =>
+        slugify(category.categorySubTypeName, { lower: true }) === categorySlug
+    );
+
+    const allProducts = filteredData.flatMap((category) => category.modelList);
+
+    return allProducts;
+  }, [data, categorySlug, isLoading]);
+
+  return { products, isLoading, error };
 };
