331a17d17db7024e3d32705d20393097b2f45d45
Robin <robinlaurentius@gmail.com>
2024-08-19 09:56:14 +0200

    feat: Add category page with dynamic rendering of category cards

--- src/pages/CategoryPage.tsx
+++ src/pages/CategoryPage.tsx
@@ -1,7 +1,43 @@
 import React from "react";
+import { useCategories } from "../hooks/useCategories";
+import { CategoryCard } from "../components/features/CategoryCard";
+import { SkeletonGrid } from "../components/skeleton/SkeletonGrid";
+import { StatusMessage } from "../components/common/StatusMessage";
+import { Grid } from "../components/layout/Grid";
+import { CenteredTitle } from "../components/layout/CenteredTitle";
+import { Container } from "../components/layout/Container";
 
-const CategoryPage = () => {
-  return <div>CategoryPage</div>;
+const CategoryPage: React.FC = () => {
+  const { categories, isLoading, error } = useCategories();
+
+  if (isLoading) {
+    return <SkeletonGrid />;
+  }
+
+  if (error) {
+    return <StatusMessage type="error" message="Something went wrong." />;
+  }
+
+  // Show the empty state if there are no categories available
+  if (!categories || categories.length === 0) {
+    return (
+      <StatusMessage
+        type="info"
+        message="No categories available at the moment."
+      />
+    );
+  }
+
+  return (
+    <Container>
+      <CenteredTitle level={1}>Our Product Lines</CenteredTitle>
+      <Grid>
+        {categories.map((category) => (
+          <CategoryCard key={category.familyId} category={category} />
+        ))}
+      </Grid>
+    </Container>
+  );
 };
 
 export default CategoryPage;
