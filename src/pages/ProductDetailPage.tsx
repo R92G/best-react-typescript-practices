9d5ace4a176c34e130c591306f163dd80858717c
Robin <robinlaurentius@gmail.com>
2024-08-19 11:06:38 +0200

    feat(detail): add Carousel and product information sections with styling

--- src/pages/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage.tsx
@@ -4,6 +4,8 @@ import { StatusMessage } from "../components/common/StatusMessage";
 import { useProductBySlug } from "../hooks/useProductBySlug";
 import { Container } from "../components/layout/Container";
 import { Banner } from "../components/features/Banner";
+import styled from "styled-components";
+import { Carousel } from "../components/features/Carousel";
 
 const ProductDetailPage: React.FC = () => {
   const { product, isLoading, isError } = useProductBySlug();
@@ -24,9 +26,70 @@ const ProductDetailPage: React.FC = () => {
     <>
       {" "}
       <Banner promotions={product.storePromotions} />
-      <Container paddingTop="10rem">{/* Content will go here */}</Container>
+      <Container paddingTop="10rem">
+        <ContentWrapper>
+          <CarouselContainer>
+            <Carousel images={product.galleryImage || []} />
+          </CarouselContainer>
+
+          <InfoContainer>
+            <ProductTitle>{product.displayName}</ProductTitle>
+            <ProductPrice>
+              {product.priceDisplay ? `€${product.priceDisplay}` : null}
+            </ProductPrice>
+          </InfoContainer>
+        </ContentWrapper>
+        ;
+      </Container>
     </>
   );
 };
 
 export default ProductDetailPage;
+
+const ContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+
+  @media (min-width: 768px) {
+    flex-direction: row;
+  }
+`;
+
+const CarouselContainer = styled.div`
+  flex: 1;
+  top: 20px;
+  height: fit-content;
+
+  @media (min-width: 768px) {
+    margin-right: 20px;
+  }
+
+  @media (min-width: 1024px) {
+    position: sticky;
+  }
+`;
+
+const InfoContainer = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  max-width: 450px;
+  background-color: ${({ theme }) => theme.colors.background};
+  padding: 20px;
+  border-radius: 12px;
+`;
+
+const ProductTitle = styled.h1`
+  font-size: 2rem;
+  margin-bottom: 10px;
+  margin-right: 20px;
+`;
+
+const ProductPrice = styled.p`
+  font-size: 1.5rem;
+  color: #007bff;
+  margin-bottom: 20px;
+`;
