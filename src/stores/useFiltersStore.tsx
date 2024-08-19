f16a13401def3476d7be84058e3624e78737b0d6
Robin <robinlaurentius@gmail.com>
2024-08-19 12:44:03 +0200

    feat: Add setFilter and applyFilters methods to useFiltersStore

--- src/stores/useFiltersStore.tsx
+++ src/stores/useFiltersStore.tsx
@@ -14,6 +14,8 @@ interface FiltersStore {
   filteredProducts: Product[];
   filters: Filters;
   setProducts: (products: Product[]) => void;
+  setFilter: <K extends keyof Filters>(filterKey: K, value: Filters[K]) => void;
+  applyFilters: (filters: Filters) => void;
 }
 
 export const useFiltersStore = create<FiltersStore>((set) => ({
@@ -31,5 +33,57 @@ export const useFiltersStore = create<FiltersStore>((set) => ({
       products,
       filteredProducts: products, // Initially, no filters applied
     }));
+  },
+  setFilter: <K extends keyof Filters>(filterKey: K, value: Filters[K]) => {
+    set((state) => ({
+      filters: { ...state.filters, [filterKey]: value },
+    }));
+  },
+  applyFilters: (newFilters: Filters) => {
+    set((state) => {
+      let updatedProducts = [...state.products];
+
+      // Apply rating filter
+      if (newFilters.minRating) {
+        const minRating = parseFloat(newFilters.minRating);
+        updatedProducts = updatedProducts.filter((product) => {
+          const rating = parseFloat(product.ratings ?? "");
+          return !isNaN(rating) && rating >= minRating;
+        });
+      }
+
+      // Apply price filters
+      const minPrice = newFilters.minPrice
+        ? parseFloat(newFilters.minPrice)
+        : 0;
+      const maxPrice = newFilters.maxPrice
+        ? parseFloat(newFilters.maxPrice)
+        : Infinity;
+
+      updatedProducts = updatedProducts.filter((product) => {
+        const price = parseFloat(product.price ?? "");
+        return !isNaN(price) && price >= minPrice && price <= maxPrice;
+      });
+
+      // Apply in-stock filter
+      if (newFilters.inStock !== null) {
+        updatedProducts = updatedProducts.filter((product) => {
+          return newFilters.inStock
+            ? product.stockStatusText === "inStock"
+            : true;
+        });
+      }
+
+      // Apply energy label filter
+      if (newFilters.energyLabel) {
+        updatedProducts = updatedProducts.filter((product) => {
+          return product.energyLabelGrade === newFilters.energyLabel;
+        });
+      }
+
+      return {
+        filteredProducts: updatedProducts,
+      };
+    });
   },
 }));
