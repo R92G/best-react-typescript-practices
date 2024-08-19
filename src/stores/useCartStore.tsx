4534ac8d25ec2c4ad821a8969460e83ae2305f20
Robin <robinlaurentius@gmail.com>
2024-08-19 12:13:24 +0200

    feat: Add addToCart function to useCartStore

--- src/stores/useCartStore.tsx
+++ src/stores/useCartStore.tsx
@@ -9,13 +9,37 @@ interface CartItem extends Product {
 
 interface CartStore {
   cartItems: CartItem[];
+  addToCart: (product: Product) => void;
 }
 
 const useCartStore = create<CartStore>()(
   persist(
     (set, get) => ({
       cartItems: [],
+      addToCart: (product: Product) => {
+        const existingItem = get().cartItems.find(
+          (item) => item.modelCode === product.modelCode
+        );
+
+        if (existingItem) {
+          // Product zit al in de winkelwagen, verhoog het aantal
+          set((state) => ({
+            cartItems: state.cartItems.map((item) =>
+              item.modelCode === product.modelCode
+                ? { ...item, quantity: item.quantity + 1 }
+                : item
+            ),
+          }));
+        } else {
+          // Product toevoegen aan de winkelwagen met een hoeveelheid van 1
+          set((state) => ({
+            cartItems: [...state.cartItems, { ...product, quantity: 1 }],
+          }));
+          toast.success(`${product.displayName} added to cart`);
+        }
+      },
     }),
+
     {
       name: "cart-storage",
     }
