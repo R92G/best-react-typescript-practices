ef5f90790c93714df178aff561aa2f0542c01a31
Robin <robinlaurentius@gmail.com>
2024-08-19 10:19:49 +0200

    feat: update ProductsPage to display products and handle loading and error states

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { ProductCard } from "../components/product/ProductCard";
+import { SkeletonGrid } from "../components/skeleton/SkeletonGrid";
+import { useProductsByCategory } from "../hooks/useProductsByCategory";
+import { Grid } from "../components/layout/Grid";
+import { CenteredTitle } from "../components/layout/CenteredTitle";
+import { Container } from "../components/layout/Container";
+import { StatusMessage } from "../components/common/StatusMessage";
 
-const ProductsPage = () => {
-  return <div>ProductsPage</div>;
+const ProductsPage: React.FC = () => {
+  const { products, isLoading, error } = useProductsByCategory();
+
+  if (isLoading && products.length === 0) {
+    return <SkeletonGrid />;
+  }
+
+  if (error || products.length === 0) {
+    return <StatusMessage type="normal" message="No products were found." />;
+  }
+
+  return (
+    <Container>
+      <CenteredTitle level={1}>Products</CenteredTitle>
+
+      <Grid>
+        {products.map((product) => (
+          <ProductCard key={product.modelCode} product={product} />
+        ))}
+      </Grid>
+    </Container>
+  );
 };
 
 export default ProductsPage;
