558eb3b032e7499f873f00c4e8582184bd2c8ac6
Robin <robinlaurentius@gmail.com>
2024-08-19 12:53:21 +0200

    feat: set filteredProducts initially to all products of the category, using useEffect. use the hook to get the filtered products and map over them in the TSX return.

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { ProductCard } from "../components/product/ProductCard";
 import { SkeletonGrid } from "../components/skeleton/SkeletonGrid";
 import { useProductsByCategory } from "../hooks/useProductsByCategory";
@@ -8,15 +8,29 @@ import { Container } from "../components/layout/Container";
 import styled from "styled-components";
 import { StatusMessage } from "../components/common/StatusMessage";
 import { BackLink } from "../components/common/BackLink";
+import { FilterButton } from "../components/common/FilterButton";
+import { useFiltersStore } from "../stores/useFiltersStore";
+import useFiltersModal from "../stores/useFiltersModal";
 
 const ProductsPage: React.FC = () => {
   const { products, isLoading, error } = useProductsByCategory();
+  const filterModal = useFiltersModal();
 
-  if (isLoading && products.length === 0) {
+  const filteredProducts = useFiltersStore((state) => state.filteredProducts);
+  const setProducts = useFiltersStore((state) => state.setProducts);
+
+  // Set products into the store when they are loaded
+  useEffect(() => {
+    if (products) {
+      setProducts(products);
+    }
+  }, [products, setProducts]);
+
+  if (isLoading && filteredProducts.length === 0) {
     return <SkeletonGrid />;
   }
 
-  if (error || products.length === 0) {
+  if (error || filteredProducts.length === 0) {
     return <StatusMessage type="normal" message="No products were found." />;
   }
 
@@ -25,10 +39,11 @@ const ProductsPage: React.FC = () => {
       <CenteredTitle level={1}>Products</CenteredTitle>
       <HeaderWrapper>
         <BackLink to="/categories" label="Categories" />
+        <FilterButton onClick={filterModal.openFilters} />
       </HeaderWrapper>
 
       <Grid>
-        {products.map((product) => (
+        {filteredProducts.map((product) => (
           <ProductCard key={product.modelCode} product={product} />
         ))}
       </Grid>
