9e59576dba1a6bf4fe617a9743a4f9dc9bb1bee9
Robin <robinlaurentius@gmail.com>
2024-08-19 11:48:18 +0200

    feat: Add HeartButton component for favoriting products

---
+++ src/components/features/HeartButton.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import styled from "styled-components";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+import useFavoritesStore from "../../stores/useFavoritesStore";
+import { Product } from "../../schemas/productSchema";
+
+interface HeartButtonProps {
+  product: Product;
+}
+
+const FilledHeart = styled(AiFillHeart)<{ favorited: boolean }>`
+  fill: ${(props) => (props.favorited ? "red" : "rgba(128, 128, 128, 0.7)")};
+`;
+
+export const HeartButton: React.FC<HeartButtonProps> = ({ product }) => {
+  const hasFavorited = useFavoritesStore((state) =>
+    state.hasFavorited(product.modelCode)
+  );
+  const toggleFavorite = useFavoritesStore((state) => state.toggleFavorite);
+
+  return (
+    <HeartContainer
+      onClick={(e) => {
+        // Prevent the click event from bubbling up to the parent element
+        e.stopPropagation();
+        e.preventDefault();
+        toggleFavorite(product);
+      }}
+    >
+      <OutlineHeart size={28} />
+      <FilledHeart size={24} favorited={hasFavorited} />
+    </HeartContainer>
+  );
+};
+
+const HeartContainer = styled.div`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  cursor: pointer;
+  transition: all 0.3s ease;
+  z-index: 5;
+
+  &:hover {
+    opacity: 0.8;
+    transform: scale(1.05);
+  }
+`;
+
+const OutlineHeart = styled(AiOutlineHeart)`
+  position: absolute;
+  top: -2px;
+  right: -2px;
+  fill: white;
+`;
