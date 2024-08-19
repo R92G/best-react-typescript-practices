2dfbe784aee0b8a9d7c221534fc358d7c1c9be4c
Robin <robinlaurentius@gmail.com>
2024-08-19 11:03:39 +0200

    feat: set up for the product detail page

--- src/pages/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { SkeletonDetail } from "../components/skeleton/SkeletonDetail";
+import { StatusMessage } from "../components/common/StatusMessage";
+import { useProductBySlug } from "../hooks/useProductBySlug";
+import { Container } from "../components/layout/Container";
 
-const ProductDetailPage = () => {
-  return <div>ProductDetailPage</div>;
+const ProductDetailPage: React.FC = () => {
+  const { product, isLoading, isError } = useProductBySlug();
+
+  if (isLoading && !product) {
+    return (
+      <Container>
+        <SkeletonDetail />
+      </Container>
+    );
+  }
+
+  if (isError || !product) {
+    return <StatusMessage message="Product niet gevonden." />;
+  }
+
+  return <></>;
 };
 
 export default ProductDetailPage;
